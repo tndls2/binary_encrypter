b9c64580c2f6e8f7417e64ed4f2c157f09ecb309
suin park <tndls3026@ajou.ac.kr>
2024-06-26 19:35:27 +0900

    [chore] response message 세분화

---
+++ src/main/java/com/example/binary_encrypter_server/dto/response/DefaultResponse.java
@@ -0,0 +1,36 @@
+package com.example.binary_encrypter_server.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class DefaultResponse<T> {
+
+    private int statusCode;
+    private String responseMessage;
+    private T data;
+
+    public DefaultResponse(final int statusCode, final String responseMessage) {
+        this.statusCode = statusCode;
+        this.responseMessage = responseMessage;
+        this.data = null;
+    }
+
+    public static <T> DefaultResponse<T> res(final int statusCode, final String responseMessage) {
+        return res(statusCode, responseMessage, null);
+    }
+
+    public static <T> DefaultResponse<T> res(final int statusCode, final String responseMessage, final T t) {
+        return DefaultResponse.<T>builder()
+                .data(t)
+                .statusCode(statusCode)
+                .responseMessage(responseMessage)
+                .build();
+    }
+}
+
+
---
+++ src/main/java/com/example/binary_encrypter_server/dto/response/EncryptionLogResponseDTO.java
@@ -0,0 +1,33 @@
+package com.example.binary_encrypter_server.dto.response;
+
+import com.example.binary_encrypter_server.domain.EncryptionLog;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EncryptionLogResponseDTO {
+    private Long id;
+    private String originFile;
+    private String encryptedFile;
+    private String iv;
+
+    private String createdAt;
+
+    public static EncryptionLogResponseDTO fromEntity(EncryptionLog log) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return new EncryptionLogResponseDTO(
+                log.getId(),
+                log.getOriginFile(),
+                log.getEncryptedFile(),
+                log.getIv(),
+                log.getCreatedAt().format(formatter)
+        );
+    }
+}
