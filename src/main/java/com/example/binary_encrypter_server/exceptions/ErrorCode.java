db7929cbfde9f25c73c348312c527ccf9cee496d
suin park <tndls3026@ajou.ac.kr>
2024-06-29 00:11:48 +0900

    [feature/exception] 예외처리

--- src/main/java/com/example/binary_encrypter_server/exceptions/ErrorCode.java
+++ src/main/java/com/example/binary_encrypter_server/exceptions/ErrorCode.java
@@ -2,7 +2,10 @@ package com.example.binary_encrypter_server.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+// 참고: https://mangkyu.tistory.com/205
 public interface ErrorCode {
+    String name();
+
     HttpStatus getHttpStatus();
 
     String getMessage();
